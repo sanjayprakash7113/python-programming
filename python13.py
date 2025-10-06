6390653d695a73f88b3304177a1b663e2309d62e
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-10-06 12:46:08 +0530

    Add files via upload

---
+++ python13.py
@@ -0,0 +1,39 @@
+mark=int(input("enter the number:"))
+
+if mark>=95:
+    print("grade a+")
+elif mark>=90:
+    print("grade a")
+elif mark>=85:
+    print("grade b+")
+elif mark>=80:
+    print("grade b")
+elif mark<=34:
+    print("re appearance")	
+else:
+    print("grad c")
+  
+
+
+for i in range(0,22):
+    if i %2==0:
+        continue
+    print(i)
+
+
+li=["sa","de","ty","er","we","wq"]
+
+for o in range(3):
+    print(li[o])
+
+a1=input("enter the input:")
+a2=input("enter the input:")
+print('\n')
+print('\n')
+for i in a1:
+    if i=="da":
+        continue
+for j in a2:
+    if j=="rr":
+        continus
+    print({a1}-{a2})
