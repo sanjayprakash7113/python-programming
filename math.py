159ca0ab7498a12a0d08686159775e2e384908f4
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-10-07 12:15:42 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,36 @@
+import math as m
+print(m.pi)
+print(m.tau)
+print(2*(m.pi))
+print(m.e)
+print(m.log(9))
+print(m.cbrt(27))
+print(m.sqrt(25))
+a1=[1,2,3,4,5,6]
+print(m.fsum(a1))
+print(m.prod(a1))
+e=3.456
+r=2.9
+print(round(e))
+print(round(r))
+print(m.ceil(e))
+print(m.fabs(e))
+print(m.factorial(6))
+
+
+
+def greet(a,d):
+    print(a+d)
+greet(2,4)
+
+
+
+
+def num(n):
+    if num>=0:
+        print("positive number")
+    elif num<=0:
+        print("negative number")
+num(-9)
+      
+
