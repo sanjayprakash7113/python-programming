66bb708fec71c5eb4acb973a61fa076da3f41a85
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-10-16 21:04:55 +0530

    Add files via upload

---
+++ inhertask.py
@@ -0,0 +1,26 @@
+class company:
+ no_of_branch=15
+ def mother_company(self):
+    print("alphat")
+ def owner(self):
+    print("share holders")
+
+class google(company):
+ no_of_department=150
+ def ceo(self):
+    print("sunder")
+ def location(self):
+    print("usa")
+
+
+class youtube(company):
+ no_of_drpartment=20
+ def ceo(self):
+    print("victor")
+ def content(self):
+    print("vedio")
+
+t1=youtube()
+t1.mother_company
+t1.owner
+t1.ceo
