50f3edfc173cb2395ab568c13f179986dcfa6484
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-10-27 12:12:54 +0530

    Add files via upload

---
+++ multilevalinher.py
@@ -0,0 +1,14 @@
+class grandpa():
+ def phone(self):
+  print("phone")
+
+class dad(grandpa):
+ def money(self):
+  print("money")
+
+class son(dad):
+ def bike(self):
+  prin("bike")
+
+victor=son()
+victor.money()
