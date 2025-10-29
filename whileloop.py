2f9d11cd49b294edf9b8eef458de79d73b8ff5e7
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-10-29 19:00:20 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,29 @@
+i=0
+while i <=10:
+    i +=1
+    print(i)
+
+i=0
+while i <=10:
+    print(i)
+    i+=1
+
+i=0
+while i <=10:
+    i+=2
+    print(i)
+i=0
+while i<=10:
+    i=i+1
+    if i %2==0:
+     countinue
+    print(1)
+
+i=0
+while i<=10:
+    i=i+1
+    if i==6:
+     break
+    print(i)
+
+
