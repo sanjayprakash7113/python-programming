ce3265b56c91f5422d5d2a803035585458d7c3d7
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-11-02 11:10:13 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,28 @@
+for i in "apple":
+    print(i)
+for i in range(1,11):
+    print(i,"x2","=",i*2)
+
+a=int(input())
+b=int(input())
+for i in range(a+1,b):
+    print(i)
+
+for i in range(1,11):
+ if i%2==0:
+    print(i)
+
+a=0
+b=0
+for i in range(1,11):
+ if i%2==0:
+    a=a+1
+ elif i%2==1:
+    b=b+1
+print("even",a)
+print("add",b)
+
+
+
+
+
