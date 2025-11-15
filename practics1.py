d97d4f7344b24ae7c7e369852a5391a84523f167
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-11-15 21:03:24 +0530

    Add files via upload

---
+++ practics1.py
@@ -0,0 +1,98 @@
+1)a=9
+if a%2==0 and a>1:
+    print(f"this is even{a}")
+elif a%2==1 and a>0:
+    print("add")
+elif a==0:
+    print("zero")
+else:
+    print("negative")
+
+2)b=("a","e","i","o","u")
+c=("hello")
+count=0
+for i in b:
+ for j in c:
+  if i==j:
+    count=count+1
+print(count)
+
+
+3)c="victor"
+u=""
+for i in c:
+ u=i+u
+print(u)
+
+4)s=(8,5,4,9,2,6)
+a=0
+b=0
+c=0
+for i in range(s[0],s):
+ if s[0]>i:
+   a=s+a
+ if s[1]>i:
+   b=s+a
+ if s[2]>i:
+   c=s+a 
+x=a,b,c
+print(x)
+
+5)a=[[12,2,3,4],[5,6],[7,5]]
+o=[x for i in a for x in i]
+print(x)
+
+
+6)s1=input("")
+s2=input("")
+for i in s1:
+ for j in s2:
+    if i==j:
+        print(True)
+
+7)h="abcdeedd"
+for i in h:
+ for j in h:
+  if i==j:
+    continue
+print(h)
+
+8)
+for i in range(1,11)
+ for j in range(2,11)
+    x=i+j
+print(x)
+
+9)no idea 
+
+10)
+w=input("")
+if w==w[::-1]:
+ print(True)
+else:
+ print(False)
+
+
+11)e=[1,2,3,4,5,6]
+
+for i in e:
+ if i%2==0 and i%3==0:
+    pass
+ elif i>1:
+    print(i)
+
+
+
+
+
+        
+ 
+ 	
+ 
+
+
+
+
+
+        
+
