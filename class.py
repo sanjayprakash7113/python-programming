4b158e092d2eddac830f8c850fc23475a4648912
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2025-11-24 23:01:12 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,27 @@
+class per_age:
+    name=" "
+    country=" "
+    date_of_birth=" "
+    def page(self):
+        self.age=date_of_birth + today.year()
+
+per_age()=old()
+
+
+class calculator():
+    value1=4
+    value2=2
+    def add(self):
+        self.a=value1+value2
+
+    def sub(self):
+        self.s=value1-value2
+
+
+    def mul(self):
+        self.m=value1*value2
+    def div(self):
+        self.d=value1/value2
+
+cal()=calculator():
+    
