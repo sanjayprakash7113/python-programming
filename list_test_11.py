6701c9b3177a63709b781aa432cb6066433cb6c8
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-01-02 21:55:00 +0530

    Add files via upload

---
+++ list_test_11.py
@@ -0,0 +1,12 @@
+n=4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()        
+
+
+n=5
+for i in range(n+1,1):
+    for j in range(i-1,1):
+        print(j,end="")
+    print()
