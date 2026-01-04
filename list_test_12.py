c20babb7151e3b79bd6782da94ccd94fd8ad2d63
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-01-04 21:21:48 +0530

    Add files via upload

---
+++ list_test_12.py
@@ -0,0 +1,29 @@
+n=4
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
+
+n=3
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
+n=4
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end="")
+    print()
+    
+n=4
+a=1
+for i in range(1,n+1):
+    for j in range(i):
+        print(a,end=" ")
+        a+=1
+    print()
+
