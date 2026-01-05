852b69ae21b464939e3606d0e72779dec5263abf
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-01-05 21:59:09 +0530

    Add files via upload

---
+++ list_test_13.py
@@ -0,0 +1,23 @@
+n=4
+for i in range(1,n):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+for i in range(n-2,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+n=4
+for i in range(1,n+1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+
+n=4
+for i in range(1,n+1):
+    for j in range(i,0,-1):
+        print("*",end=
+              " ")
+    print()
+    
