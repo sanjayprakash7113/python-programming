d06f38832ad5ef5f33bc53dd71d7bb9fd33c67de
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-01-19 21:48:48 +0530

    Add files via upload

---
+++ leetcode_test_1.py
@@ -0,0 +1,27 @@
+a=[4,3,2,9]
+b=1
+for i in range(len(a)-1,-1,-1):
+    a[i]+=b
+    if a[i]>=10:
+        a[i]=0
+        b=1
+    else:
+        b=0
+        break
+print(a)
+
+digits=[1,2,3]
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        a=1
+        for i in range(len(digits)-1,-1,-1):
+            digits[i]+=a
+            if digits[i]>=10:
+                digits[i]=0
+                a=1
+            else:
+                a=0
+                break
+print(digits)
+       
+    
