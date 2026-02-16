4307732a96b14c93c46c92961968d750d5545e20
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-02-16 22:26:26 +0530

    Add files via upload

--- data_type_test_13.py
+++ data_type_test_13.py
@@ -9,4 +9,25 @@ if len(nums)!=len(num):
     num.append(-1)
 print(num)
 
+s = "abcabcbb"
+b=0
+c=[]
+for i in s:
+    if i not in c:
+        c.append(i)
+        b+=1
+print(b)
 
+
+
+nums = [2,2,1,1,1,2,2]
+b={}
+for i in nums:
+    if i in b:
+        b[i]+=1
+    else:
+        b[i]=1
+a=len(nums)/2
+for k,v in b.items():
+    if v>a:
+        print(k)
