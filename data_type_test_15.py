69f8987026caf6657c52661ffb7b7f90d212040b
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-02-22 20:09:41 +0530

    Add files via upload

--- data_type_test_15.py
+++ data_type_test_15.py
@@ -6,3 +6,36 @@ for i in range(len(height)):
         if res>a:
             a=res
 print(a*a)
+
+
+n=4
+for i in range(1,n):
+    for j in range(i):
+        print("*",end= " ")
+    print()
+
+n=6
+for i in range(1,n):
+    for j in range(i,n-1):
+        print("*",end=" ")
+    print()
+
+n=7
+for i in range(n):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+
+n = 5
+for i in range(1, n+1):
+    print(" " * (n - i),end="")   # spaces
+    for j in range(i):
+        print("* ",end="")
+    print()
+
+
+
+
+
+
+
