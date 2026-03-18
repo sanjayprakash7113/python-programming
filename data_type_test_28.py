ba3df70dd06a9dea81c32d191f248606a83d4fb5
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-03-18 21:28:33 +0530

    Add files via upload

---
+++ data_type_test_28.py
@@ -0,0 +1,50 @@
+nums = [[1,3], [2,6], [8,10], [15,18]]
+nums.sort()
+res=[nums[0]]
+
+for st,en in nums[1:]:
+    last=res[-1][1]
+    if st<=last:
+        res[-1][1]=max(last,en)
+    else:
+        res.append([st,en])
+print(res)
+
+
+
+nums=[100,2,3,4]
+k=3
+k=k%len(nums)
+res=nums[-k:]+nums[:-k]
+print(res)
+
+nums=[2,3,2,3,5]
+for i in range(len(nums)):
+    if nums.count(nums[i])==1:
+        print(nums[i])
+        break
+
+
+nums = [1,2,3,4,5,6]
+k = 3
+
+su_m=sum(nums[:k])
+ma_x=su_m
+for i in range(k,len(nums)):
+    su_m+=nums[i]
+    su_m-=nums[i-k]
+    ma_x=max(su_m,ma_x)
+print(ma_x)
+
+
+
+s = "abcabcbb"
+for i in s:
+    cur=""
+    if i not in cur:
+        cur+=i
+    else:
+        break
+print(len(cur))
+
+
