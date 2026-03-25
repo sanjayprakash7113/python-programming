497cdcc943bfccdf3f6cfd8240b494d12aa62036
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-03-25 21:16:11 +0530

    Add files via upload

---
+++ data_type_test_31.py
@@ -0,0 +1,17 @@
+height = [1,8,6,2,5,4,8,3,7]
+mx=0
+for i in range(len(height)):
+    for j in range(i+1,len(height)):
+        mul=(min(height[i],height[j]))*(j-i)
+        mx=max(mx,mul)
+print(mx)
+        
+
+
+
+
+
+
+
+
+        
